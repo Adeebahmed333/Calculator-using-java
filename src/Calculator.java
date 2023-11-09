dcb7c979e186e31a39ea0c6ffcfa6d0fdc89a5f8
Adeeb Ahmed <adeebahmed3337@gmail.com>
2023-11-09 13:20:18 +0530

    Remove Bugs and Improved UI

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,45 +4,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame implements ActionListener {
-    // create a frame
+   
     static JFrame frame;
 
-    // create a textfield
+    
     static JTextField textField;
 
-    // store operator and operands
+    
     String operator;
     double num1, num2, result;
 
-    // default constructor
+    
     public Calculator() {
     }
 
-    // main function
+   
     public static void main(String[] args) {
-        // create a frame
+       
         frame = new JFrame("Calculator");
 
         try {
-            // set look and feel
+            
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
-        // create a object of class
+        
         Calculator c = new Calculator();
 
-        // create a textfield
+       
         textField = new JTextField(16);
 
-        // set the textfield to non editable
+        
         textField.setEditable(false);
 
-        // create number buttons and some operators
+        
         JButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonAdd, buttonSub, buttonMul, buttonDiv, buttonEqual, buttonClear, buttonDot;
 
-        // create number buttons
+        
         button0 = new JButton("0");
         button1 = new JButton("1");
         button2 = new JButton("2");
@@ -54,9 +54,9 @@ public class Calculator extends JFrame implements ActionListener {
         button8 = new JButton("8");
         button9 = new JButton("9");
 
-        // equals button
+        
         buttonEqual = new JButton("=");
-        // create operator buttons
+      
         buttonAdd = new JButton("+");
         buttonSub = new JButton("-");
         buttonMul = new JButton("*");
@@ -64,10 +64,10 @@ public class Calculator extends JFrame implements ActionListener {
         buttonClear = new JButton("C");
         buttonDot = new JButton(".");
 
-        // create a panel
+        
         JPanel p = new JPanel();
 
-        // add action listeners
+       
         button0.addActionListener(c);
         button1.addActionListener(c);
         button2.addActionListener(c);
@@ -86,7 +86,7 @@ public class Calculator extends JFrame implements ActionListener {
         buttonEqual.addActionListener(c);
         buttonClear.addActionListener(c);
 
-        // add elements to panel
+      
         p.add(textField);
         p.add(button1);
         p.add(button2);
@@ -106,10 +106,10 @@ public class Calculator extends JFrame implements ActionListener {
         p.add(buttonDiv);
         p.add(buttonClear);
 
-        // set Background of panel
+       
         p.setBackground(Color.BLACK);
 
-        // add panel to frame
+        
         frame.add(p);
 
         frame.setSize(200, 220);
